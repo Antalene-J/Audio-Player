e266d41611ea08d5f3bc8297e723848a294195e1
Antalene_J <119392158+Antalene-J@users.noreply.github.com>
2023-11-15 20:53:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,28 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
+import json
+import random
 
 PORT = 8000
 Handler = SimpleHTTPRequestHandler
 
-with TCPServer(("", PORT), Handler) as httpd:
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/generate_random_indices':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            # Replace 48 with the total number of songs in your playlist
+            indices = random.sample(range(48), 48)
+
+            # Send the random indices as JSON
+            self.wfile.write(json.dumps(indices).encode())
+        else:
+            # Serve other requests using the default handler
+            super().do_GET()
+
+with TCPServer(("", PORT), MyHandler) as httpd:
     print("Server started at localhost:" + str(PORT))
     print("link: http://localhost:8000")
     httpd.serve_forever()
